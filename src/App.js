26978899bc38dddf894e63f714f7dac987b193db
Michael Guerrero <micgurro@gmail.com>
2018-06-11 13:07:43 -0400

    Created a React To Do List App.
    -Made this as a learner app.
    -Will be used as the starting point for a data visualization app.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from "./Components/Layout";
+
+class App extends Component {
+  
+  render() {
+    
+    return (
+      
+        <Layout />
+     
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/TodoItems.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default class TodoItems extends React.Component {
+
+    constructor(props) {
+        super(props); 
+
+        this.createTasks = this.createTasks.bind(this);
+
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+    createTasks(item) {
+        return<li onClick ={() => this.delete(item.key)}
+                    key= {item.key}>{item.text}</li>
+    }
+    render(){
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+
+        return(
+            <ul className = "theList">
+                {listItems}
+            </ul>
+        );
+    }
+
+}
